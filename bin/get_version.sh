c15376829484a87c09dbe45e29ad817dd37e15a9
Emmett Kotlikoff <ekotlikoff@gmail.com>
2022-06-19 21:22:46 +0100

    Default the version to 0.1.0 if not tagged

--- bin/get_version.sh
+++ bin/get_version.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-git describe --tags --abbrev=0 > version.txt
+tag=`git describe --tags --abbrev=0 2>/dev/null`
+RESULT=$?
+if [ "$RESULT" -eq 0 ]; then
+  echo "$tag" > version.txt
+else
+  echo "0.1.0" > version.txt
+fi
